ab3b84f59f69e8ebf686b59d8d8e468660bff892
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-04-03 23:05:30 -0400

    1614 maxDepth problem

---
+++ MaxDepth/Solution.java
@@ -0,0 +1,23 @@
+package MaxDepth;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        int depth = MaxDepth(s);
+        System.out.println(depth);
+    }
+
+    private static int MaxDepth(String s) {
+        int maxDepth = 0;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+            maxDepth = Math.max(maxDepth, count);
+        }
+        return maxDepth;
+    }
+}
