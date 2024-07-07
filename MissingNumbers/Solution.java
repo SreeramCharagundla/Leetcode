c71b35943248872ec631bf5705e10775bffc74f5
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-07-06 21:51:35 -0400

    Missing Numbers

---
+++ MissingNumbers/Solution.java
@@ -0,0 +1,12 @@
+package MissingNumbers;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        n = (n*(n+1))/2;
+        for(int i=0;i<nums.length;i++){
+            n-=nums[i];
+        }
+        return n;
+    }
+}
