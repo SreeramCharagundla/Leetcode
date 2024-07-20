e763241a4977baa0ed1e59fe2c6c46d600f78977
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-07-19 21:57:03 -0400

    leetcode 136 single number

---
+++ singleNumber/Solution.java
@@ -0,0 +1,11 @@
+package singleNumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
