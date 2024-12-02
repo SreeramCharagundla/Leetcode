0a54970b70dc7a252adf365681d712e67f33bfd7
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-12-02 18:40:23 -0500

    initial commit for the leetcode question - permutations - recursive backtracking

---
+++ Permutations/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> answer = permute(nums);
+        System.out.println(answer);
+        
+    }        
+    
+    public static List<List<Integer>> permute(int[] nums) {
+        
+        
+        
+        return new ArrayList<List<Integer>>();
+    }
+}
