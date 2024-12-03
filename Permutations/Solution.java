933da71fb66a7ccbb811fad0302f5ed6acf0c901
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-12-02 21:42:27 -0500

    backtrack recursion

--- Permutations/Solution.java
+++ Permutations/Solution.java
@@ -9,9 +9,22 @@ public class Solution{
     }        
     
     public static List<List<Integer>> permute(int[] nums) {
-        
-        
-        
-        return new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(new ArrayList<>(),ans,nums);
+        return ans;
+    }
+
+    public static void backtrack(List<Integer> curr, List<List<Integer>> ans, int[] nums){
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int num:nums){
+            if(!curr.contains(num)){
+                curr.add(num);
+                backtrack(curr,ans,nums);
+                curr.remove(curr.size()-1);
+            }
+        }
     }
 }
