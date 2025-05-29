57f70bc97937bc9f421a1d06118981264a7eb8d8
Sreeram Charagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2025-05-29 00:26:45 -0400

    Merge sorted array question Leetcode

--- MergeSortedArray/Solution.java
+++ MergeSortedArray/Solution.java
@@ -4,11 +4,25 @@ class Solution{
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
-        System.out.println("here");
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        
+        for(int i : nums1)
+        System.out.print( i+" ");
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums1copy = Arrays.copyOf(nums1,m);
+        int idx = 0;
+        int i=0,j=0;
 
+        while(idx<m+n){
+            if(j>=n || (i<m && nums1copy[i]<nums2[j])){
+                nums1[idx++] = nums1copy[i++];
+            }else{
+                nums1[idx++] = nums2[j++]; 
+            }
+        }
     }
 }
