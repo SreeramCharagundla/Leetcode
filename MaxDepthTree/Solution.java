e7e98e8e957243af82405a0af6b773808afce77f
Sreeram Charagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2025-10-18 14:37:29 -0400

    finding max depth of a binary tree using recursion DFS. Love the solution

--- MaxDepthTree/Solution.java
+++ MaxDepthTree/Solution.java
@@ -1,5 +1,41 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){}
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val=val;
+        this.left=left;
+        this.right=right;
+    }
+}
+
 public class Solution {
+
+    static int maxDepth(TreeNode root){
+        if(root==null){
+            return 0;        
+        }else{ 
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+
+            return java.lang.Math.max(left_height,right_height)+1;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20,new TreeNode(15),new TreeNode(7));
+
+        int depth = maxDepth(root);
+        System.out.println("Max Depth of the Binary Tree is "+depth);
     }    
 }
